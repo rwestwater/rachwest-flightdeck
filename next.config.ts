04b3909b3e3c7f81a240d529f5d0d41d50efffce
Rachel Westwater <rachelwestwater@Rachels-MacBook-Pro.local>
2026-04-22 12:12:20 +0100

    Rename project to rachwest-flightdeck

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export", // Enable static export for GitHub Pages
-  basePath: "/rachwist-flightdeck", // Set the base path to your repo name
+  basePath: "/rachwest-flightdeck", // Set the base path to your repo name
   trailingSlash: true, // Add trailing slashes to URLs
 };
 

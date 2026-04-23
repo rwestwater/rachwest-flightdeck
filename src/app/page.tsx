f677e909acfa48abafce1d0665d6c1308cadbfb4
Rachel Westwater <rachel-westwater@hotmail.com>
2026-04-23 15:00:28 +0100

    Fix JSX syntax error - remove extra closing div

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -90,7 +90,6 @@ export default function Home() {
                 </div>
               </a>
             </div>
-            </div>
           </div>
 
           <div className="mt-12 text-center">
